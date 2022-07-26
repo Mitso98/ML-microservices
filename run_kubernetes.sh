63af9103bf9c3759ad6fceb8ecbbc7c52b9ffbd9
EC2 Default User <ec2-user@ip-172-31-7-114.ec2.internal>
2022-07-26 01:21:46 +0000

    containrized app

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mo9348/ml-microservices"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservices \
+    --image=$dockerpath \
+    --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-microservices 3000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="mo9348/ml-microservices"
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,6 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker login --username=mo9348
+docker tag ml-microservices:latest $dockerpath
+docker push $dockerpath
